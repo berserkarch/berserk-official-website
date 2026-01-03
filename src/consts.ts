c9170372daaf010ada226d93887306e5c13eec63
Gaurav Raj <techw803@gmail.com>
2026-01-03 22:23:19 +0530

    updated things x2

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE =
-  "Berserk Arch - Opinionated Cybersecurity & Development Linux Distro based on Arch Linux";
+  "Berserk Arch - Cybersecurity & Development Linux Distro based on Arch Linux";
 export const SITE_DESCRIPTION =
-  "Berserk Arch is a bleeding-edge, security-centric, opinionated Arch-based Linux distribution crafted for hackers, developers, and nerds alike.";
+  "Berserk Arch is a bleeding-edge, security-centric Arch-based Linux distribution crafted for hackers, developers, and nerds alike.";
